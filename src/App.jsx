8150dcf3838162462ee1102a8814dc7a5cd42bf5
Ayush Chaurasiya <23je0194@iitism.ac.in>
2025-06-28 21:09:44 +0530

    New Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Route, Routes } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import Home from "./pages/Home";
 import AddTask from "./pages/AddTask";
 import EditTask from "./pages/EditTask";
@@ -9,7 +9,23 @@ import Header from "./components/Header";
 import Account from "./pages/AccountPage";
 
 const App = () => {
-  const isAuth = !!localStorage.getItem("token");
+  const [isAuth, setIsAuth] = useState(!!localStorage.getItem("token"));
+  const navigate = useNavigate();
+
+  // Sync auth state when token is updated in localStorage
+  useEffect(() => {
+    const handleStorageChange = () => {
+      setIsAuth(!!localStorage.getItem("token"));
+    };
+
+    window.addEventListener("storage", handleStorageChange);
+    return () => window.removeEventListener("storage", handleStorageChange);
+  }, []);
+
+  // Optional: watch token on every navigation
+  useEffect(() => {
+    setIsAuth(!!localStorage.getItem("token"));
+  }, [window.location.pathname]);
 
   return (
     <>
@@ -19,9 +35,9 @@ const App = () => {
           <Route path="/" element={isAuth ? <Home /> : <Login />} />
           <Route path="/add" element={isAuth ? <AddTask /> : <Login />} />
           <Route path="/edit/:id" element={isAuth ? <EditTask /> : <Login />} />
-          <Route path="/account" element={<Account />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
+          <Route path="/account" element={isAuth ? <Account /> : <Login />} />
+          <Route path="/login" element={!isAuth ? <Login /> : <Home />} />
+          <Route path="/register" element={!isAuth ? <Register /> : <Home />} />
         </Routes>
       </div>
     </>
